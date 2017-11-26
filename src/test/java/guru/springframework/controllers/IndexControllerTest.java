f508ea121fcf21cf40becbc44b15706926020ea3
mcsimk <33173318+mcsimk@users.noreply.github.com>
2017-11-26 11:45:14 +0000

    test circle ci

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -74,7 +74,7 @@ public class IndexControllerTest {
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         Set<Recipe> setInController = argumentCaptor.getValue();
-        assertEquals(2, setInController.size());
+        assertEquals(3, setInController.size());
     }
 
 }
